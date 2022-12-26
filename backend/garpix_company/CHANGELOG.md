3bf5d884e8b9d739bcf1c5d69ca6b0151d3ec4b3
griviala <78097561+griviala@users.noreply.github.com>
2022-12-26 15:00:45 +0200

    1.0.0-rc5

--- backend/garpix_company/CHANGELOG.md
+++ backend/garpix_company/CHANGELOG.md
@@ -1,7 +1,12 @@
+### 1.0.0-rc5 (26.12.2022)
+
+- Admin panel changes
+
 ### 1.0.0-rc4 (08.12.2022)
 
 - `destroy` action added to `UerCompany` view.
 - `AbstractUserCompanyRole` model added (see `Readme.md`).
+- `check_user_companies_limit` class method added to `Company` model.
 
 ### 1.0.0-rc3 (02.12.2022)
 
--- backend/garpix_company/admin/company.py
+++ backend/garpix_company/admin/company.py
@@ -6,9 +6,11 @@ from garpix_company.models import UserCompany
 class UserCompanyInline(admin.TabularInline):
     model = UserCompany
     extra = 0
+    raw_id_fields = ['user']
 
 
 class CompanyAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
     readonly_fields = ('created_at', )
     inlines = (UserCompanyInline,)
+    raw_id_fields = ['owner']
